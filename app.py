4273a853ed28ca276e11b925cfd8361c80c727c0
kholidfu <sopier@gmail.com>
2016-11-08 11:33:20 +0700

    from imgpath to urlpath, renamce

--- app.py
+++
@@ -1,5 +0,0 @@
-from colorweave import palette
-
-imgpath = '/Users/macbookpro/Desktop/colorwooden.jpg'
-
-print(palette(path=imgpath, n=6, format="css3"))
---
+++ example.py
@@ -0,0 +1,4 @@
+from colorweave import palette
+
+urlpath = 'http://st.hzcdn.com/simgs/43119d07045c50e0_3-2972/modern-kitchen.jpg'
+print(palette(url=urlpath, n=6, format="css3"))
