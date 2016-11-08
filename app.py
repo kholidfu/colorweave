f68c01afb26459704094bf320a6e7cbba5a9f6d6
kholidfu <sopier@gmail.com>
2016-11-08 11:25:40 +0700

    support for python 3.5

---
+++ app.py
@@ -0,0 +1,5 @@
+from colorweave import palette
+
+imgpath = '/Users/macbookpro/Desktop/colorwooden.jpg'
+
+print(palette(path=imgpath, n=6, format="css3"))
--- colorweave.py
+++ colorweave.py
@@ -1,19 +1,28 @@
+import sys
 from collections import Counter, namedtuple, OrderedDict
 from operator import itemgetter, mul, attrgetter
+import colorsys
 import webcolors
-from urllib2 import urlopen
+try:
+    from urllib.request import urlopen
+except:
+    from urllib2 import urlopen
 from PIL import Image as Im
 from PIL import ImageChops, ImageDraw
-from colormath.color_objects import RGBColor
-import cStringIO
+from colormath import color_diff
+from colormath.color_objects import sRGBColor, LabColor
+from colormath.color_conversions import convert_color
+import io
 import json
-import colorsys
-
+import random
+from math import sqrt
 
 Color = namedtuple('Color', ['value', 'prominence'])
+Palette = namedtuple('Palette', 'colors bgcolor')
+Point = namedtuple('Point', ('coords', 'n', 'ct'))
+Cluster = namedtuple('Cluster', ('points', 'center', 'n'))
 
 convert3To21 = {"indigo": "purple", "gold": "orange", "firebrick": "red", "indianred": "red", "yellow": "yellow", "darkolivegreen": "green", "darkseagreen": "green", "mediumvioletred": "pink", "mediumorchid": "purple", "chartreuse": "green", "mediumslateblue": "purple", "black": "black", "springgreen": "green", "orange": "orange", "lightsalmon": "red", "brown": "brown", "turquoise": "teal", "olivedrab": "green", "cyan": "cyan", "silver": "gray", "skyblue": "blue", "darkturquoise": "teal", "goldenrod": "brown", "darkgreen": "green", "darkviolet": "purple", "darkgray": "gray", "lightpink": "pink", "teal": "teal", "darkmagenta": "purple", "lightgoldenrodyellow": "yellow", "lavender": "purple", "yellowgreen": "green", "thistle": "purple", "violet": "purple", "navy": "blue", "dimgrey": "gray", "orchid": "purple", "blue": "blue", "ghostwhite": "white", "honeydew": "white", "cornflowerblue": "blue", "darkblue": "blue", "darkkhaki": "yellow", "mediumpurple": "purple", "cornsilk": "brown", "red": "red", "bisque": "brown", "slategray": "gray", "darkcyan": "teal", "khaki": "yellow", "wheat": "brown", "deepskyblue": "blue", "darkred": "red", "steelblue": "blue", "aliceblue": "white", "lightslategrey": "gray", "gainsboro": "gray", "mediumturquoise": "teal", "floralwhite": "white", "coral": "orange", "aqua": "cyan", "burlywood": "brown", "darksalmon": "red", "beige": "white", "azure": "white", "lightsteelblue": "blue", "oldlace": "white", "greenyellow": "green", "fuchsia": "purple", "lightseagreen": "teal", "mistyrose": "white", "sienna": "brown", "lightcoral": "red", "orangered": "orange", "navajowhite": "brown", "lime": "green", "palegreen": "green", "lightcyan": "cyan", "seashell": "white", "mediumspringgreen": "green", "royalblue": "blue", "papayawhip": "yellow", "blanchedalmond": "brown", "peru": "brown", "aquamarine": "cyan", "white": "white", "darkslategray": "gray", "lightgray": "gray", "ivory": "white", "dodgerblue": "blue", "lawngreen": "green", "chocolate": "brown", "crimson": "red", "forestgreen": "green", "slateblue": "purple", "olive": "green", "mintcream": "white", "antiquewhite": "white", "hotpink": "pink", "moccasin": "yellow", "limegreen": "green", "saddlebrown": "brown", "grey": "gray", "darkslateblue": "purple", "lightskyblue": "blue", "deeppink": "pink", "plum": "purple", "darkgoldenrod": "brown", "maroon": "maroon", "sandybrown": "brown", "tan": "brown", "magenta": "purple", "rosybrown": "brown", "pink": "pink", "lightblue": "blue", "palevioletred": "pink", "mediumseagreen": "green", "linen": "white", "darkorange": "orange", "powderblue": "blue", "seagreen": "green", "snow": "white", "mediumblue": "blue", "midnightblue": "blue", "paleturquoise": "cyan", "palegoldenrod": "yellow", "whitesmoke": "white", "darkorchid": "purple", "salmon": "red", "lemonchiffon": "yellow", "lightgreen": "green", "tomato": "orange", "cadetblue": "teal", "lightyellow": "yellow", "lavenderblush": "white", "purple": "purple", "mediumaquamarine": "cyan", "green": "green", "blueviolet": "purple", "peachpuff": "yellow"}
-
 
 def prepare_output(colors, format):
     ''' Prepares the output determined by what format is given. If no format, then list of hex codes is returned '''
@@ -60,19 +69,17 @@ def prepare_output(colors, format):
                 output['tree'][convert3To21[name]].append({name : color})
         return output
 
-
 def closest_color(requested_color):
     ''' Find the name of the closest color given a requested color. '''
 
     min_colors = {}
     for key, name in webcolors.css3_hex_to_names.items():
-        r_c, g_c, b_c = hex_to_rgb(key)
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
         rd = (r_c - requested_color[0]) ** 2
         gd = (g_c - requested_color[1]) ** 2
         bd = (b_c - requested_color[2]) ** 2
         min_colors[(rd + gd + bd)] = name
     return min_colors[min(min_colors.keys())]
-
 
 def get_color_name(requested_color):
     ''' Get the name of a color (either according to CSS3 or CSS2.1). If exact color cannot be mapped, this method finds the closest color. '''
@@ -84,21 +91,20 @@ def get_color_name(requested_color):
         actual_name = None
     return closest_name
 
-
 def distance(c1, c2):
     ''' Calculate the visual distance between the two colors. '''
-    return RGBColor(*c1).delta_e(RGBColor(*c2), method='cmc')
-
+    labColor1 = convert_color(sRGBColor(*c1), LabColor)
+    labColor2 = convert_color(sRGBColor(*c2), LabColor)
+    return color_diff.delta_e_cie2000(labColor1, labColor2)
 
 def rgb_to_hex(color):
     ''' Convert from RGB to Hex. '''
     return '#%.02x%.02x%.02x' % color
 
-
 def hex_to_rgb(color):
     ''' Convert from Hex to RGB. '''
+    assert color.startswith('#') and len(color) == 7
     return (int(color[1:3], 16), int(color[3:5], 16), int(color[5:7], 16))
-
 
 def extract_colors(imageData, n, format, output):
     """
@@ -135,7 +141,7 @@ def extract_colors(imageData, n, format, output):
     # aggregate colors
     to_canonical = {WHITE: WHITE, BLACK: BLACK}
     aggregated = Counter({WHITE: 0, BLACK: 0})
-    sorted_cols = sorted(dist.iteritems(), key=itemgetter(1), reverse=True)
+    sorted_cols = sorted(dist.items(), key=itemgetter(1), reverse=True)
     for c, n in sorted_cols:
         if c in aggregated:
             # exact match!
@@ -153,7 +159,7 @@ def extract_colors(imageData, n, format, output):
 
     # order by prominence
     colors = sorted((Color(c, n / float(n_pixels)) \
-                for (c, n) in aggregated.iteritems()),
+                for (c, n) in aggregated.items()),
             key=attrgetter('prominence'),
             reverse=True)
 
@@ -182,6 +188,9 @@ def extract_colors(imageData, n, format, output):
     else:
         return prepare_output(final_colors_hex, format)
 
+def norm_color(c):
+    r, g, b = c
+    return (r/255.0, g/255.0, b/255.0)
 
 def detect_background(im, colors, to_canonical):
     BACKGROUND_PROMINENCE = 0.5
@@ -207,15 +216,8 @@ def detect_background(im, colors, to_canonical):
 
     return colors, bg_color
 
-
-def norm_color(c):
-    r, g, b = c
-    return (r/255.0, g/255.0, b/255.0)
-
-
 def meets_min_saturation(c, threshold):
     return colorsys.rgb_to_hsv(*norm_color(c.value))[1] > threshold
-
 
 def autocrop(im, bgcolor):
     ''' Crop away a border of the given background color.'''
@@ -227,9 +229,93 @@ def autocrop(im, bgcolor):
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
-    else:
-        return im
 
+    return im # no contents, don't crop to nothing
+
+def get_points(img):
+    ''' Get all the points given an image. '''
+
+    points = []
+    w, h = img.size
+    for count, color in img.getcolors(w * h):
+        points.append(Point(color, 3, count))
+    return points
+
+# Lambda function to convert RGB to Hex
+rtoh = lambda rgb: '#%s' % ''.join(('%02x' % p for p in rgb))
+
+def colorz(imageData, n, format, output):
+    ''' Main function to find the color palette using k-means clustering method. '''
+
+    img = Im.open(imageData)
+    img.thumbnail((200, 200)) # Resize the image for faster processing
+    w, h = img.size
+
+    if n:
+        n = int(n)
+    else:
+        n = 5
+
+    points = get_points(img) # Get all the points in an image
+    clusters = kmeans(points, n, 10) # Find the clusters in an image, given n number of clusters and difference among the clusters
+    rgbs = [map(int, c.center.coords) for c in clusters]
+
+    # Get the colors
+    final_colors_hex = []
+    for each_color in map(rtoh, rgbs):
+        final_colors_hex.append(each_color)
+
+    # Produce the output
+    if output == 'json':
+        return json.dumps(prepare_output(final_colors_hex, format), indent=4)
+    else:
+        return prepare_output(final_colors_hex, format)
+
+def euclidean(p1, p2):
+    ''' Get the euclidean distance between two points. '''
+    return sqrt(sum([
+        (p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)
+    ]))
+
+def calculate_center(points, n):
+    vals = [0.0 for i in range(n)]
+    plen = 0
+    for p in points:
+        plen += p.ct
+        for i in range(n):
+            vals[i] += (p.coords[i] * p.ct)
+    return Point([(v / plen) for v in vals], n, 1)
+
+def kmeans(points, k, min_diff):
+    ''' Method to perform k-means clustering on the image points with k-clusters. '''
+
+    #Form the clusters given k
+    clusters = [Cluster([p], p, p.n) for p in random.sample(points, k)]
+
+    while 1:
+        plists = [[] for i in range(k)]
+
+        for p in points:
+            smallest_distance = float('Inf')
+            for i in range(k):
+                distance = euclidean(p, clusters[i].center)
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    idx = i
+            plists[idx].append(p)
+
+        diff = 0
+        for i in range(k):
+            old = clusters[i]
+            center = calculate_center(plists[i], old.n)
+            new = Cluster(plists[i], center, old.n)
+            clusters[i] = new
+            diff = max(diff, euclidean(old.center, new.center))
+
+        if diff < min_diff:
+            break
+    #Return all the clusters
+    return clusters
 
 def palette(**kwargs):
     # Parse all the options
@@ -243,12 +329,19 @@ def palette(**kwargs):
     # If the image is given as a URL
     if url:
         imageFile = urlopen(url)
-        imageData = cStringIO.StringIO(imageFile.read())
-        return extract_colors(imageData, n, format, output)
+        # imageData = cStringIO.StringIO(imageFile.read())
+        imageData = io.BytesIO(imageFile.read()) # Could actually be BytesIO()
+        if not mode:
+            return extract_colors(imageData, n, format, output)
+        elif mode.lower() == 'kmeans' or mode.lower() == 'k-means':
+            return colorz(imageData, n, format, output)
     # If image is given as a local file path
     elif path:
-        return extract_colors(path, n, format, output)
+        if not mode:
+            return extract_colors(path, n, format, output)
+        elif mode.lower() == 'kmeans' or mode.lower() == 'k-means':
+            return colorz(path, n, format, output)
     # Unknown format of image
     else:
-        print "Unable to get image. Exiting."
-        return None
+        print("Unable to get image. Exiting.")
+        sys.exit(0)
